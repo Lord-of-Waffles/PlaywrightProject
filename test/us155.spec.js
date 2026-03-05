32816efc3fe664f7778416e51e76c09a6f88aaef
Dehlya <dehlya.studio@gmail.com>
2026-03-05 13:03:15 +0100

    ADD US155 - filter results by cat

--- test/us155.spec.js
+++ test/us155.spec.js
@@ -1,1 +1,12 @@
 // As a user I want to filter my results by category
+import { test, expect } from '@playwright/test';
+
+test('filter results by category', async () => {
+
+    // step 1 : select areas
+
+    // step 2 : select layers 
+
+    // step 3 : open the results tab   
+
+});

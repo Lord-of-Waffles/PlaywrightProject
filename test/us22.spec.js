0321b39903046176b55b35c9d55d0b054a595569
Lord-of-Waffles <ben.worton@gmail.com>
2026-03-12 10:22:10 +0100

    Finished US22

--- test/us22.spec.js
+++ test/us22.spec.js
@@ -1,1 +1,58 @@
+//@ts-check
 // As a user I want to register on the platform
+
+import { test, expect } from '@playwright/test';
+
+
+// test that we can access the maps page
+test('Go to maps', async ({ page }) => {
+    await page.goto('https://citiwattsdev.hevs.ch/');
+
+    await page.getByRole('button', { name: 'Go to app' }).click();
+
+    await expect(page).toHaveURL('https://citiwattsdev.hevs.ch/map')
+});
+
+
+
+// test
+test('Navigate to Login page', async ({ page }) => {
+
+    await test.step('Navigate to login', async () => {
+
+        await page.goto('https://citiwattsdev.hevs.ch/map');
+
+        // Dismiss cookie dialog if present
+
+        const declineCookies = page.getByRole("button", {name : "Decline statistics cookies"});
+        if (await declineCookies.isVisible({ timeout: 5000 }).catch(() => false)) {
+            await declineCookies.click();
+        }
+
+        //await page.getByLabel('Login').click();
+
+        await page.getByRole('button', { name: 'Login' }).click();
+    });
+
+    await test.step('Fill form', async () => {
+
+        await page.getByRole('link', { name: 'Register' }).click();
+
+        await page.getByLabel('First name').fill('James');
+
+        await page.getByLabel('Last name').fill('Jameson');
+
+        await page.getByLabel('Email').fill('ben.worton@gmail.com');
+
+        await page.locator('#password').fill('Affection6-Smuggler2-Gully8');
+
+        await page.getByLabel('Confirm password').fill('Affection6-Smuggler2-Gully8');
+
+        await page.getByLabel('I agree to the terms and conditions').check();
+
+        await page.getByRole('button', { name: 'Register' }).click();
+
+        await expect(page).toHaveTitle(/Sign in to Toolbox Login/)
+    });
+
+});
